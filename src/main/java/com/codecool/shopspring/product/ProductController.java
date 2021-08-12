6ecd78a89710100b091345c666eb18f57ab9b222
Łukasz Ściga <lukasz.sciga@codecool.com>
2021-08-12 21:47:42 +0200

    Add restcontroller for products and basic getmapping and requestparam examples

---
+++ src/main/java/com/codecool/shopspring/product/ProductController.java
@@ -0,0 +1,27 @@
+package com.codecool.shopspring.product;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    @GetMapping("/products")
+    public List<String> getProducts() {
+        return List.of("iPhone 11", "iPhone 12 Turbo x");
+    }
+
+    @GetMapping("/branded-products")
+    public List<String> getProductRequestParam(@RequestParam(required = false) final String brand) {
+        if (brand != null) {
+            if (brand.equals("iphone")) {
+                return List.of("iPhone 11");
+            } else {
+                return List.of("Samsung");
+            }
+        } else {
+            return List.of("iPhone 11", "Samsung");
+        }
+    }
+}
